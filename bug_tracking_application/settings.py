be3fcad1e75a6dafc9deaa906b9cb2514135d467
Nirmal Kushwah <nirmalkushwah2010@gmail.com>
2024-05-09 10:57:19 +0530

    updated project

--- bug_tracking_application/settings.py
+++ bug_tracking_application/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-nz&2#lotvu_2kr!z$r7ka84+y8sz_sh!z=ta0m2*3yzfm+xfct
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
